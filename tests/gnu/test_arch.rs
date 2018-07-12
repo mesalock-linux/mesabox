325fbc2f71403ee876f35dcda8bd6e4511326dac
Mingshen Sun <mssun@mesalock-linux.org>
2018-07-11 17:17:06 -0700

    tests/gnu/arch: add tests

--- tests/gnu/test_arch.rs
+++ tests/gnu/test_arch.rs
@@ -1,7 +1,33 @@
 //
 // Copyright (c) 2018, The MesaLock Linux Project Contributors
 // All rights reserved.
-// 
+//
 // This work is licensed under the terms of the BSD 3-Clause License.
 // For a copy, see the LICENSE file.
 //
+
+use util::*;
+
+#[test]
+#[cfg(target_arch = "x86_64")]
+fn test_x86_64() {
+    new_ucmd!()
+        .succeeds()
+        .stdout_only("x86_64\n");
+}
+
+#[test]
+#[cfg(target_arch = "arm")]
+fn test_arm() {
+    new_ucmd!()
+        .succeeds()
+        .stdout_only("arm\n");
+}
+
+#[test]
+#[cfg(target_arch = "aarch64")]
+fn test_aarch64() {
+    new_ucmd!()
+        .succeeds()
+        .stdout_only("aarch64\n");
+}
