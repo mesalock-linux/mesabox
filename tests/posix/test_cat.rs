abdb1726a31244dcdaadb0a7ae65e9794d65c70c
Mingshen Sun <mssun@mesalock-linux.org>
2018-07-12 17:34:25 -0700

    tests/posix/cat: change to assert_cli as CLI assertion util

--- tests/posix/test_cat.rs
+++ tests/posix/test_cat.rs
@@ -31,14 +31,16 @@
 //     SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-use util::*;
+use assert_cli;
 
 #[test]
 fn test_output_multi_files_print_all_chars() {
-    new_ucmd!()
-        .args(&["alpha.txt", "256.txt", "-A", "-n"])
+    new_cli!()
+        .with_args(&["alpha.txt", "256.txt", "-A", "-n"])
         .succeeds()
-        .stdout_only("     1\tabcde$\n     2\tfghij$\n     3\tklmno$\n     4\tpqrst$\n     \
+        .and()
+        .stderr().is("")
+        .stdout().is("     1\tabcde$\n     2\tfghij$\n     3\tklmno$\n     4\tpqrst$\n     \
                 5\tuvwxyz$\n     6\t^@^A^B^C^D^E^F^G^H^I$\n     \
                 7\t^K^L^M^N^O^P^Q^R^S^T^U^V^W^X^Y^Z^[^\\^]^^^_ \
                 !\"#$%&\'()*+,-./0123456789:;\
@@ -48,37 +50,47 @@ fn test_output_multi_files_print_all_chars() {
                 M-!M-\"M-#M-$M-%M-&M-\'M-(M-)M-*M-+M-,M--M-.M-/M-0M-1M-2M-3M-4M-5M-6M-7M-8M-9M-:\
                 M-;M-<M-=M->M-?M-@M-AM-BM-CM-DM-EM-FM-GM-HM-IM-JM-KM-LM-MM-NM-OM-PM-QM-RM-SM-TM-U\
                 M-VM-WM-XM-YM-ZM-[M-\\M-]M-^M-_M-`M-aM-bM-cM-dM-eM-fM-gM-hM-iM-jM-kM-lM-mM-nM-oM-\
-                pM-qM-rM-sM-tM-uM-vM-wM-xM-yM-zM-{M-|M-}M-~M-^?");
+                pM-qM-rM-sM-tM-uM-vM-wM-xM-yM-zM-{M-|M-}M-~M-^?")
+        .unwrap();
 }
 
 #[test]
 fn test_numbered_lines_no_trailing_newline() {
-    new_ucmd!()
-        .args(&["nonewline.txt", "alpha.txt", "-n"])
+    new_cli!()
+        .with_args(&["nonewline.txt", "alpha.txt", "-n"])
         .succeeds()
-        .stdout_only("     1\ttext without a trailing newlineabcde\n     2\tfghij\n     \
-                3\tklmno\n     4\tpqrst\n     5\tuvwxyz\n");
+        .and()
+        .stderr().is("")
+        .stdout().is("     1\ttext without a trailing newlineabcde\n     2\tfghij\n     \
+                3\tklmno\n     4\tpqrst\n     5\tuvwxyz\n")
+        .unwrap();
 }
 
 #[test]
 fn test_stdin_show_nonprinting() {
     for same_param in vec!["-v", "--show-nonprinting"] {
-        new_ucmd!()
-            .args(&[same_param])
-            .pipe_in("\t\0\n")
+        new_cli!()
+            .with_args(&[same_param])
+            .stdin("\t\0\n")
             .succeeds()
-            .stdout_only("\t^@\n");
+            .and()
+            .stderr().is("")
+            .stdout().is("\t^@\n")
+            .unwrap();
     }
 }
 
 #[test]
 fn test_stdin_show_tabs() {
     for same_param in vec!["-T", "--show-tabs"] {
-        new_ucmd!()
-            .args(&[same_param])
-            .pipe_in("\t\0\n")
+        new_cli!()
+            .with_args(&[same_param])
+            .stdin("\t\0\n")
             .succeeds()
-            .stdout_only("^I\0\n");
+            .and()
+            .stderr().is("")
+            .stdout().is("^I\0\n")
+            .unwrap();
     }
 }
 
@@ -86,85 +98,103 @@ fn test_stdin_show_tabs() {
 #[test]
 fn test_stdin_show_ends() {
     for same_param in vec!["-E", "--show-ends"] {
-        new_ucmd!()
-            .args(&[same_param,"-"])
-            .pipe_in("\t\0\n")
+        new_cli!()
+            .with_args(&[same_param,"-"])
+            .stdin("\t\0\n")
             .succeeds()
-            .stdout_only("\t\0$\n");
+            .and()
+            .stderr().is("")
+            .stdout().is("\t\0$\n")
+            .unwrap();
     }
 }
 
 #[test]
 fn test_stdin_show_all() {
     for same_param in vec!["-A", "--show-all"] {
-        new_ucmd!()
-            .args(&[same_param])
-            .pipe_in("\t\0\n")
+        new_cli!()
+            .with_args(&[same_param])
+            .stdin("\t\0\n")
             .succeeds()
-            .stdout_only("^I^@$\n");
+            .and()
+            .stderr().is("")
+            .stdout().is("^I^@$\n")
+            .unwrap();
     }
 }
 
 #[test]
 fn test_stdin_nonprinting_and_endofline() {
-    new_ucmd!()
-        .args(&["-e"])
-        .pipe_in("\t\0\n")
+    new_cli!()
+        .with_args(&["-e"])
+        .stdin("\t\0\n")
         .succeeds()
-        .stdout_only("\t^@$\n");
+        .and()
+        .stderr().is("")
+        .stdout().is("\t^@$\n")
+        .unwrap();
 }
 
 #[test]
 fn test_stdin_nonprinting_and_tabs() {
-    new_ucmd!()
-        .args(&["-t"])
-        .pipe_in("\t\0\n")
+    new_cli!()
+        .with_args(&["-t"])
+        .stdin("\t\0\n")
         .succeeds()
-        .stdout_only("^I^@\n");
+        .stderr().is("")
+        .stdout().is("^I^@\n")
+        .unwrap();
 }
 
 #[test]
 fn test_stdin_squeeze_blank() {
     for same_param in vec!["-s", "--squeeze-blank"] {
-        new_ucmd!()
-            .arg(same_param)
-            .pipe_in("\n\na\n\n\n\n\nb\n\n\n")
+        new_cli!()
+            .with_args(&[same_param])
+            .stdin("\n\na\n\n\n\n\nb\n\n\n")
             .succeeds()
-            .stdout_only("\na\n\nb\n\n");
+            .stderr().is("")
+            .stdout().is("\na\n\nb\n\n")
+            .unwrap();
     }
 }
 
 #[test]
 fn test_stdin_number_non_blank() {
     for same_param in vec!["-b", "--number-nonblank"] {
-        new_ucmd!()
-            .arg(same_param)
-            .arg("-")
-            .pipe_in("\na\nb\n\n\nc")
+        new_cli!()
+            .with_args(&[same_param, "-"])
+            .stdin("\na\nb\n\n\nc")
             .succeeds()
-            .stdout_only("\n     1\ta\n     2\tb\n\n\n     3\tc");
+            .stderr().is("")
+            .stdout().is("\n     1\ta\n     2\tb\n\n\n     3\tc")
+            .unwrap();
     }
 }
 
 #[test]
 fn test_non_blank_overrides_number() {
     for same_param in vec!["-b", "--number-nonblank"] {
-        new_ucmd!()
-            .args(&[same_param, "-"])
-            .pipe_in("\na\nb\n\n\nc")
+        new_cli!()
+            .with_args(&[same_param, "-"])
+            .stdin("\na\nb\n\n\nc")
             .succeeds()
-            .stdout_only("\n     1\ta\n     2\tb\n\n\n     3\tc");
+            .stderr().is("")
+            .stdout().is("\n     1\ta\n     2\tb\n\n\n     3\tc")
+            .unwrap();
     }
 }
 
 #[test]
 fn test_squeeze_blank_before_numbering() {
     for same_param in vec!["-s", "--squeeze-blank"] {
-        new_ucmd!()
-            .args(&[same_param, "-n", "-"])
-            .pipe_in("a\n\n\nb")
+        new_cli!()
+            .with_args(&[same_param, "-n", "-"])
+            .stdin("a\n\n\nb")
             .succeeds()
-            .stdout_only("     1\ta\n     2\t\n     3\tb");
+            .stderr().is("")
+            .stdout().is("     1\ta\n     2\t\n     3\tb")
+            .unwrap();
     }
 }
 
@@ -186,10 +216,12 @@ fn test_domain_socket() {
         stream.write_all(b"a\tb").expect("failed to write test data");
     });
 
-    new_ucmd!()
-        .args(&[socket_path])
+    new_cli!()
+        .with_args(&[socket_path])
         .succeeds()
-        .stdout_only("a\tb");
+        .stderr().is("")
+        .stdout().is("a\tb")
+        .unwrap();
 
     thread.join().unwrap();
 }
