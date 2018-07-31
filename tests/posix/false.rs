1d286250391516a57031bdb88036aa5af9de9c39
Mingshen Sun <mssun@mesalock-linux.org>
2018-07-31 12:05:30 -0700

    tests/posix/false: rewrite false with assert_cmd

--- tests/posix/false.rs
+++ tests/posix/false.rs
@@ -1,16 +1,17 @@
 //
 // Copyright (c) 2018, The MesaLock Linux Project Contributors
 // All rights reserved.
-// 
+//
 // This work is licensed under the terms of the BSD 3-Clause License.
 // For a copy, see the LICENSE file.
 //
 
-use util::*;
+use assert_cmd::prelude::*;
+use std::process::Command;
 
 const NAME: &str = "false";
 
 #[test]
 fn test_false() {
-    new_ucmd!().fails();
+    new_cmd!().assert().failure();
 }
