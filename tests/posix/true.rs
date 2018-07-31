2be41067f85cfd4cc93f2e9b4be22d20a97fb832
Mingshen Sun <mssun@mesalock-linux.org>
2018-07-31 12:51:34 -0700

    tests/posix/true: rewrite true with assert_cmd

--- tests/posix/true.rs
+++ tests/posix/true.rs
@@ -1,16 +1,17 @@
 //
 // Copyright (c) 2018, The MesaLock Linux Project Contributors
 // All rights reserved.
-// 
+//
 // This work is licensed under the terms of the BSD 3-Clause License.
 // For a copy, see the LICENSE file.
 //
 
-use util::*;
+use assert_cmd::prelude::*;
+use std::process::Command;
 
 const NAME: &str = "true";
 
 #[test]
 fn test_true() {
-    new_ucmd!().succeeds();
+    new_cmd!().assert().success();
 }
